1f4f787de64aead7faa94e067453fc75a73bad6e
Collin OConnell <ococncol@gmail.com>
2017-09-13 11:50:16 -0400

    tried to change View? to ListView? but it failed to compile

--- app/src/main/java/design/shortnd/todolist/MainActivity.kt
+++ app/src/main/java/design/shortnd/todolist/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.ListView
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*

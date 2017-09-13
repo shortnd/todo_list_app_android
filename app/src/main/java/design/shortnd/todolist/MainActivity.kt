33cce4ff93e1877ef85e5a72c1086bf192c06db7
Collin OConnell <ococncol@gmail.com>
2017-09-13 11:48:18 -0400

    implemented ToDoAdapter to display the todo list to the listview, along with adding a margin around the listView to make it look a little less squished

--- app/src/main/java/design/shortnd/todolist/MainActivity.kt
+++ app/src/main/java/design/shortnd/todolist/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
 /*
 * Copyright (C) 2017 The Android Open Source Project
@@ -30,5 +32,22 @@ class MainActivity : AppCompatActivity() {
         add_new_todo_fab.setOnClickListener {
             startActivity(Intent(this, AddNewTodoActivity::class.java))
         }
+        getRealmDatabase()
+    }
+
+    private fun getRealmDatabase() {
+        // Gets a default instance of Realm Database
+        val realm = Realm.getDefaultInstance()
+        // Queries the whole database
+        val query = realm.where(ToDoItem::class.java)
+        // Gets the results for all items in the database
+        val results = query.findAll()
+        // Gets the todoListView
+        val todoListView = findViewById<ListView>(R.id.todo_list_view)
+        // makes an array adapter for the list view
+        val adapter = ToDoAdapter(this, android.R.layout.simple_list_item_1, results)
+        // Connects the adapter to the list view
+        todoListView.adapter = adapter
+
     }
 }
---
+++ app/src/main/java/design/shortnd/todolist/ToDoAdapter.kt
@@ -0,0 +1,46 @@
+package design.shortnd.todolist
+
+import android.R.layout
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Typeface
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+class ToDoAdapter(context: Context?, resource: Int, objects: MutableList<ToDoItem>?) :
+        ArrayAdapter<ToDoItem>(context, resource, objects) {
+    override fun getCount(): Int {
+        return super.getCount()
+    }
+
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val toDoTextView = inflater.inflate(layout.simple_list_item_1, parent, false) as TextView
+        val toDoItem = getItem(position)
+        toDoTextView.text = toDoItem.name
+        if (toDoItem.important) {
+            toDoTextView.typeface = Typeface.DEFAULT_BOLD
+        }
+        return toDoTextView
+    }
+}
